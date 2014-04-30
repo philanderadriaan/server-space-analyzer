667e88b5c7f7967fe1bcd40c82a10c0c17127c15
philanderadriaan <philander90@msn.com>
2014-04-30 12:11:36 -0700

    Added GUI labels.

--- Server Space Analyzer/Form1.Designer.cs
+++ Server Space Analyzer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Server_Space_Analyzer
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.username_label = new System.Windows.Forms.Label();
+            this.password_label = new System.Windows.Forms.Label();
+            this.username_textbox = new System.Windows.Forms.TextBox();
+            this.password_textbox = new System.Windows.Forms.TextBox();
+            this.ok_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username_label
+            // 
+            this.username_label.AutoSize = true;
+            this.username_label.Location = new System.Drawing.Point(10, 10);
+            this.username_label.Name = "username_label";
+            this.username_label.Size = new System.Drawing.Size(35, 13);
+            this.username_label.TabIndex = 0;
+            this.username_label.Text = "Username";
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Location = new System.Drawing.Point(10, 40);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(35, 13);
+            this.password_label.TabIndex = 1;
+            this.password_label.Text = "Password";
+            // 
+            // username_textbox
+            // 
+            this.username_textbox.Location = new System.Drawing.Point(70, 10);
+            this.username_textbox.Name = "username_textbox";
+            this.username_textbox.Size = new System.Drawing.Size(100, 20);
+            this.username_textbox.TabIndex = 2;
+            // 
+            // password_textbox
+            // 
+            this.password_textbox.Location = new System.Drawing.Point(70, 40);
+            this.password_textbox.Name = "password_textbox";
+            this.password_textbox.Size = new System.Drawing.Size(100, 20);
+            this.password_textbox.TabIndex = 3;
+            // 
+            // ok_button
+            // 
+            this.ok_button.Location = new System.Drawing.Point(52, 70);
+            this.ok_button.Name = "ok_button";
+            this.ok_button.Size = new System.Drawing.Size(75, 23);
+            this.ok_button.TabIndex = 4;
+            this.ok_button.Text = "OK";
+            this.ok_button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(180, 100);
+            this.Controls.Add(this.ok_button);
+            this.Controls.Add(this.password_textbox);
+            this.Controls.Add(this.username_textbox);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.username_label);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+            this.CenterToScreen();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label username_label;
+        private System.Windows.Forms.Label password_label;
+        private System.Windows.Forms.TextBox username_textbox;
+        private System.Windows.Forms.TextBox password_textbox;
+        private System.Windows.Forms.Button ok_button;
     }
 }
-
--- Server Space Analyzer/Program.cs
+++ Server Space Analyzer/Program.cs
@@ -16,7 +16,7 @@ namespace Server_Space_Analyzer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
